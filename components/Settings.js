4e1cba4e9ad13b80991ee0e806ee5fa4addf888d
Mouhanedbr <rhouma30@outlook.com>
2020-03-12 19:48:18 -0700

    finished the design for the settings page

--- components/Settings.js
+++ components/Settings.js
@@ -8,58 +8,199 @@ import {
   Dimensions,
   Image
 } from "react-native";
+import { Card, ListItem, Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default class Settings extends React.Component {
-  constructor() {
-    super();
-  }
-
   render() {
-    const contentChange = () => {};
-
     return (
-      <View style={style.container}>
-        <Text>Settings Page</Text>
-
-        <TouchableOpacity style={style.button}>
-          <Text style={style.myText}>Click here to search</Text>
-        </TouchableOpacity>
-
-        <Text style={style.myText}>OR</Text>
-
-        <TextInput
-          style={style.textInput}
-          placeholder="Enter a song's title, artist or lyrics here!"
-          onChangeText={text => this.setState({ text })}
-          value={this.state.text}
-        />
-
-        <TouchableOpacity onPress={contentChange}>
-          <Text style={style.container}>This is my button</Text>
-        </TouchableOpacity>
+      <View style={styles.container}>
+        <Text style={styles.title}>Settings</Text>
+        <Card
+          containerStyle={{ height: 130, width: 344, borderRadius: 50 }}
+          title="Language"
+        >
+          <View style={styles.buttons}>
+            <Icon style={[{ color: "black" }]} size={35} name={"translate"} />
+            <Button
+              type="outline"
+              buttonStyle={styles.buttonStyle}
+              title="EN"
+            />
+            <Button
+              type="outline"
+              buttonStyle={styles.buttonStyle}
+              title="FR"
+            />
+            <Button
+              type="outline"
+              buttonStyle={styles.buttonStyle}
+              title="CH"
+            />
+          </View>
+        </Card>
+        <Card
+          containerStyle={{ height: 130, width: 344, borderRadius: 50 }}
+          title="Results Per Page"
+        >
+          <View style={styles.buttons}>
+            <Icon
+              style={[{ color: "black" }]}
+              size={35}
+              name={"book-open-page-variant"}
+            />
+            <Button type="outline" buttonStyle={styles.buttonStyle} title="5" />
+            <Button
+              type="outline"
+              buttonStyle={styles.buttonStyle}
+              title="15"
+            />
+            <Button
+              type="outline"
+              buttonStyle={styles.buttonStyle}
+              title="20"
+            />
+          </View>
+        </Card>
+        <Card
+          containerStyle={{ height: 240, width: 344, borderRadius: 50 }}
+          title="Text Settings"
+        >
+          <View
+            style={
+              ({ flex: 1 },
+              { flexDirection: "column" },
+              { justifyContent: "space-between" })
+            }
+          >
+            <View style={styles.buttons}>
+              <Icon
+                style={[{ color: "black" }]}
+                size={35}
+                name={"format-font"}
+              />
+              <Button
+                type="outline"
+                buttonStyle={styles.buttonStyle}
+                title="Font1"
+              />
+              <Button
+                type="outline"
+                buttonStyle={styles.buttonStyle}
+                title="Font2"
+              />
+              <Button
+                type="outline"
+                buttonStyle={styles.buttonStyle}
+                title="Font3"
+              />
+            </View>
+            <View style={styles.buttons}>
+              <Icon
+                style={[{ color: "black" }]}
+                size={35}
+                name={"format-color-text"}
+              />
+              <Button
+                type="outline"
+                buttonStyle={styles.buttonStyle}
+                title="Black"
+              />
+              <Button
+                type="outline"
+                buttonStyle={styles.buttonStyle}
+                title="Blue"
+              />
+              <Button
+                type="outline"
+                buttonStyle={styles.buttonStyle}
+                title="Red"
+              />
+            </View>
+            <View style={styles.buttons}>
+              <Icon
+                style={[{ color: "black" }]}
+                size={35}
+                name={"format-size"}
+              />
+              <Button
+                type="outline"
+                buttonStyle={styles.buttonStyle}
+                title="4"
+              />
+              <Button
+                type="outline"
+                buttonStyle={styles.buttonStyle}
+                title="8"
+              />
+              <Button
+                type="outline"
+                buttonStyle={styles.buttonStyle}
+                title="12"
+              />
+            </View>
+          </View>
+        </Card>
+        <View style={styles.buttons}>
+          <Button
+            buttonStyle={({ borderColor: "red" }, { width: 120 })}
+            title="Terms"
+            icon={<Icon name="message" size={20} color="white" />}
+          />
+          <Button
+            buttonStyle={({ borderRadius: 100 }, { width: 120 })}
+            title="About"
+            icon={<Icon name="information-outline" size={25} color="white" />}
+          />
+        </View>
       </View>
     );
   }
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    padding: 20
-  },
-  button: {
-    marginBottom: 30,
-    width: Dimensions.get("window").width - 30,
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-evenly",
     alignItems: "center",
-    backgroundColor: "#36AF4E",
-    Image: "./Images/search.png"
+    backgroundColor: "#f6e0ce"
   },
-
-  textInput: {
-    padding: 40
+  title: {
+    fontSize: 39,
+    color: "#707070",
+    borderColor: "#707070",
+    alignItems: "center",
+    justifyContent: "flex-start"
   },
-
-  myText: {
-    fontSize: 20,
-    textAlign: "center"
+  language: {
+    height: 90,
+    width: 344,
+    borderRadius: 50,
+    backgroundColor: "#FFFFFF",
+    borderColor: "#010101",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  },
+  text: {
+    height: 285,
+    width: 344,
+    borderRadius: 50,
+    backgroundColor: "#FFFFFF",
+    borderColor: "#010101",
+    alignItems: "center"
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center"
+  },
+  buttonStyle: {
+    borderRadius: 50,
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+    height: 50,
+    width: 50
   }
 });
